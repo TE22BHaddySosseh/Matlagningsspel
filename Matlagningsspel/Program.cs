ad2e7a2b2a38b91e0c20a0dbad605d12e8de34cb
Haddy Sosseh <Haddy.Sosseh@elev.ga.ntig.se>
2025-05-21 13:01:07 +0200

    commit 7

--- Matlagningsspel/Program.cs
+++ Matlagningsspel/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+Game game = new();
+
+game.Start();
+
 public class Game
 {
     private static Random random = new();
@@ -15,17 +19,18 @@ public class Game
 
     public void Start()
     {
-        
+
         while (true)
         {
             Console.WriteLine("Welcome to the ultimate cooking show! Your job is to make a dish our judges will simply rave over!");
             Console.WriteLine("Get your knives and cutting board ready, chef!");
-
+            Console.WriteLine("Choose ingredients for your dish! Here's a list:");
+            
             Pot pot = new();
             List<Judge> judges = GenerateJudges(3);
-            Console.WriteLine("Choose ingredients for your dish! Here's a list:");
-            foreach (var name in ingredientNames){
-        
+            foreach (var name in ingredientNames)
+            {
+
                 Console.WriteLine("- " + name);
             }
 
@@ -34,24 +39,28 @@ public class Game
             {
                 Console.Write("Enter your ingredient name to get cookin'! (or type 'done' to finish!): ");
                 string input = Console.ReadLine();
-                
-                if (input == null){
+
+                if (input == null)
+                {
                     Console.WriteLine("Please enter a valid ingredient!");
                     continue;
                 }
 
-                if (input == "done"){
+                if (input == "done")
+                {
                     done = true;
                     continue;
                 }
 
                 Ingredient ingredientToAdd = new Ingredient(input);
                 bool added = false;
-                if (pot.AddIngredient(ingredientToAdd)){
+                if (pot.AddIngredient(ingredientToAdd))
+                {
                     added = true;
                 }
 
-                if (!added){
+                if (!added)
+                {
                     Console.WriteLine("Oops, not enough budget!");
                 }
 
@@ -59,32 +68,34 @@ public class Game
 
             int totalscore = 0;
             Console.WriteLine("Judge score!:");
-            foreach (var Judge in judges){
+            foreach (var Judge in judges)
+            {
                 int score = Judge.EvaluateDish(pot);
-                Console.WriteLine(Judge.name + " likes " + Judge.preferredIngredient + " and gave your dish " + score);
+                Console.WriteLine(Judge.Name + " likes " + Judge.PreferredIngredient + " and gave your dish " + score);
                 totalscore += score;
             }
 
-            int averagescore = totalscore/judges.Count;
+            int averagescore = totalscore / judges.Count;
             Console.WriteLine("Your average score is: " + averagescore);
 
             Console.Write("Play another round? (y/n): ");
             string playAgain = Console.ReadLine();
-            if (playAgain != "yes" && playAgain != "y"){
+            if (playAgain != "yes" && playAgain != "y")
+            {
                 break;
             }
         }
     }
 
-    public List<Judge> GenerateJudges(int Count){
+    public List<Judge> GenerateJudges(int Count)
+    {
         List<Judge> judges = new();
+        List<string> copynames = new(JudgesNames);
         for (int i = 1; 1 <= Count; i++)
         {
             string name = $"Judge {i}";
             string preferredIngredient = ingredientNames[random.Next(ingredientNames.Count)];
-            Random nimnam = new Random(); 
-            int index = nimnam.Next(judges.Count);
-            judges.Add(new Judge(name,preferredIngredient));
+            judges.Add(new Judge(name, preferredIngredient));
         }
         return judges;
     }
